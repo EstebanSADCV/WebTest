e3144f16468275781ce5de73a88988e16a3ab09b
Esteban Lira <etsbansonic_1998@hotmail.com>
2021-08-06 23:26:23 -0500

    test start

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import logo201 from "./logo201.png"
 
 const Home = () => {
     return (
@@ -6,6 +7,7 @@ const Home = () => {
             <h1 className="text-white">
             Aqui es Home
             </h1>
+            <img src={logo201} alt="logo" />
         </div> 
     )
 }
