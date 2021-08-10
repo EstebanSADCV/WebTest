d9a0d1e8c6c166e2b0dc6aa1140da511259ae86a
Esteban Lira <etsbansonic_1998@hotmail.com>
2021-08-10 00:15:00 -0500

    Start 4

--- src/componentes/Info.jsx
+++ src/componentes/Info.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import logo512x420 from "./logo512x420.png"
+import Equipacion1 from "./Equipacion1.png"
+import Equipacion2 from "./Equipacion2.png"
+import Equipacion3 from "./Equipacion3.png"
+
 
 const Info = () => {
     return (
@@ -6,6 +11,18 @@ const Info = () => {
             <h1 className="text-white">
             Informacion sobre la App
             </h1>
+            <p>
+                <img src={logo512x420} alt="logo" />
+            </p>
+            <p>
+                <img src={Equipacion1} alt="logo" />
+            </p>
+            <p>
+            <img src={Equipacion2} alt="logo" />
+            </p>
+            <p>
+            <img src={Equipacion3} alt="logo" />
+            </p>
         </div> 
     )
 }
